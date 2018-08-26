22f0658875011f2d2923a7e21302ce1164d44c2f
Pichayuth Kittisriswai <pichayuthkitti@gmail.com>
2018-08-26 21:10:51 +0700

    add cookie session to auth

--- services/passport.js
+++ services/passport.js
@@ -1,10 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-
 const User = mongoose.model('users');
 
 const keys = require('../config/keys');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
