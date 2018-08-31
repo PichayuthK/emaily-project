3d8c36177995b876edc17ac207e5ed7e6c9e2c27
Pichayuth Kittisriswai <pichayuthkitti@gmail.com>
2018-08-31 23:37:19 +0700

    add handler to SurveyFormReview

--- client/src/components/Surveys/SurveyFormReview.js
+++ client/src/components/Surveys/SurveyFormReview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const SurveyReview = () => {
+const SurveyFormReview = (props) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      <button className="yellow darken-2 btn-flat" onClick={props.onCancel}>
+        Back
+      </button>
     </div>
   );
 };
 
-export default SurveyReview;
+export default SurveyFormReview;
