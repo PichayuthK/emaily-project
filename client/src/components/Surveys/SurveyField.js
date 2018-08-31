b4949c070a84b7ada2aa680849572301a53ba0c2
Pichayuth Kittisriswai <pichayuthkitti@gmail.com>
2018-08-31 21:39:55 +0700

    add validation to form

--- client/src/components/Surveys/SurveyField.js
+++ client/src/components/Surveys/SurveyField.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const SurveyField = (props) => {
-  const { input, label } = props;
+  const {
+    input,
+    label,
+    meta: { error, touched },
+  } = props;
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{ marginBottom: '5px' }} />
+      <div className="red-text" style={{ marginBottom: '20px' }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
