753107670439353e9bfe5a8f3e8c2e76567dce15
Pichayuth Kittisriswai <pichayuthkitti@gmail.com>
2018-08-31 22:07:14 +0700

    add validation for invalid email

--- client/src/components/Surveys/SurveyForm.js
+++ client/src/components/Surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmail';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -51,6 +52,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
