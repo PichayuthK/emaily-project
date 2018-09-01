8a8ead9cb932d6440c101cfe165d30a9ae405c1a
Pichayuth Kittisriswai <pichayuthkitti@gmail.com>
2018-09-01 07:48:42 +0700

    add show review form in SurveyFormReview

--- client/src/components/Surveys/SurveyFormReview.js
+++ client/src/components/Surveys/SurveyFormReview.js
@@ -1,13 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import formFields from './formFields';
+import * as actions from '../../actions';
+
 const SurveyFormReview = (props) => {
-  const { onCancel, formValues } = props;
+  const { onCancel, formValues, submitSurvey } = props;
+
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-2 btn-flat" onClick={onCancel}>
+      {reviewFields}
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        className="green btn-flat white-text right"
+        onClick={() => submitSurvey(formValues)}
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -17,4 +42,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(SurveyFormReview);
